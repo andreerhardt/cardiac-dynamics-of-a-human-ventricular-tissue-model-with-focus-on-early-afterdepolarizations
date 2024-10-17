c8617961b6157ddf869b4883f0be68c72ee66cb4
André H. Erhardt <44664645+andreerhardt@users.noreply.github.com>
2024-10-17 18:12:18 +0200

    Update plotlimitcycle.m

--- bifurcation analysis/plotlimitcycle.m
+++ bifurcation analysis/plotlimitcycle.m
@@ -1,3 +1,4 @@
+%% author: André H. Erhardt (andre.erhardt@wias-berlin.de, https://orcid.org/0000-0003-4389-8554)
 function res = plotlimitcycle(x,v,s,e,color,EdgeAlpha,LineStyle,FaceAlpha)
 
 global lds cds
