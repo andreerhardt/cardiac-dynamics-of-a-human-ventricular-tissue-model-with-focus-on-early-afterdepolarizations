328c3d7ed7c311dabf91483e54f2a50578697e6e
André H. Erhardt <44664645+andreerhardt@users.noreply.github.com>
2024-10-17 12:16:41 +0200

    Add files via upload

---
+++ comparison plots/fun_modified_TP06_epi_M_endo_16d.m
@@ -0,0 +1,172 @@
+function dydt = fun_eval(t,y,par_g_Kr,par_g_Ks,par_g_Na,par_g_K1,par_g_CaL,par_K_i,stim_amplitude,choice)
+%% variables
+V       = y(1); 
+Xr1     = y(2); 
+Xr2     = y(3); 
+Xs      = y(4); 
+m       = y(5); 
+d       = y(6); 
+f       = y(7); 
+f2      = y(8); 
+fCass   = y(9);
+s       = y(10); 
+r       = y(11); 
+R_prime = y(12); 
+Ca_i    = y(13); 
+Ca_SR   = y(14); 
+Ca_ss   = y(15); 
+Na_i    = y(16);
+%% parameters
+R        = 8314.472;
+T        = 310;
+F        = 96485.3415;
+V_c      = 0.016404;
+P_kna    = 0.03;
+Cm       = 1;
+g_bna    = 0.00029;
+g_bca    = 0.000592;
+if choice == 0
+% epi and M cell
+    g_to = 0.294;
+elseif choice == 1
+% endo
+    g_to = 0.073;
+end
+%
+g_pCa    = 0.1238;
+g_pK     = 0.0146;
+K_o      = 5.4;
+Na_o     = 140;
+K_pCa    = 0.0005;
+P_NaK    = 2.724;
+K_mk     = 1;
+K_mNa    = 40;
+K_NaCa   = 1000;
+K_sat    = 0.1;
+alpha_0  = 2.5;
+gamma_0  = 0.35;
+Km_Ca    = 1.38;
+Km_Nai   = 87.5;
+Ca_o     = 2;
+k1_prime = 0.15;
+k2_prime = 0.045;
+k3       = 0.06;
+k4       = 0.005;
+EC       = 1.5;
+max_sr   = 2.5;
+min_sr   = 1;
+V_rel    = 0.102;
+V_xfer   = 0.0038;
+K_up     = 0.00025;
+V_leak   = 0.00036;
+Vmax_up  = 0.006375;
+Buf_c    = 0.2;
+K_buf_c  = 0.001;
+Buf_sr   = 10;
+K_buf_sr = 0.3;
+Buf_ss   = 0.4;
+K_buf_ss = 0.00025;
+V_sr     = 0.001094;
+V_ss     = 0.00005468;
+%% reversal potentials
+E_Na = R*T/F*log(Na_o/Na_i);
+E_K  = R*T/F*log(K_o/par_K_i);
+E_Ks = R*T/F*log((K_o+P_kna*Na_o)/(par_K_i+P_kna*Na_i));
+E_Ca = 0.5*R*T/F*log(Ca_o/Ca_i);
+%% ion currents 
+% Inward Rectifier Potassium Current
+alpha_K1    = 0.1/(1+exp(0.06*(V-E_K-200)));
+beta_K1     = (3*exp(0.0002*(V-E_K+100))+exp(0.1*(V-E_K-10)))/(1+exp(-0.5*(V-E_K)));
+xK1_inf     = alpha_K1/(alpha_K1+beta_K1);
+i_K1        = par_g_K1*xK1_inf*sqrt(K_o/5.4)*(V-E_K);
+% Rapid Delayed Rectifier Current
+xr1_inf     = 1/(1+exp((-26-V)/7));
+alpha_xr1   = 450/(1+exp((-45-V)/10));
+beta_xr1    = 6/(1+exp((V+30)/11.5));
+tau_xr1     = alpha_xr1*beta_xr1;
+xr2_inf     = 1/(1+exp((V+88)/24));
+alpha_xr2   = 3/(1+exp((-60-V)/20));
+beta_xr2    = 1.12/(1+exp((V-60)/20));
+tau_xr2     = alpha_xr2*beta_xr2;
+i_Kr        = par_g_Kr*sqrt(K_o/5.4)*Xr1*Xr2*(V-E_K);
+% Slow Delayed Rectifier Current
+xs_inf      = 1/(1+exp((-5-V)/14));
+alpha_xs    = 1400/sqrt(1+exp((5-V)/6));
+beta_xs     = 1/(1+exp((V-35)/15));
+tau_xs      = alpha_xs*beta_xs+80;
+i_Ks        = par_g_Ks*Xs^2*(V-E_Ks);
+% Fast Sodium Current
+m_inf       = 1/(1+exp((-56.86-V)/9.03))^2;
+alpha_m     = 1/(1+exp((-60-V)/5)); 
+beta_m      = 0.1/(1+exp((V+35)/5))+0.1/(1+exp((V-50)/200));
+tau_m       = alpha_m*beta_m;
+v_inf       = 1/(1+exp((V+71.55)/7.43))^2;
+i_Na        = par_g_Na*m^3*v_inf^2*(V-E_Na);
+% L-type Calcium Current
+d_inf       = 1/(1+exp((-8-V)/7.5));
+alpha_d     = 1.4/(1+exp((-35-V)/13))+0.25;
+beta_d      = 1.4/(1+exp((V+5)/5));
+gamma_d     = 1/(1+exp((50-V)/20));
+tau_d       = alpha_d*beta_d+gamma_d;
+f_inf       = 1/(1+exp((V+20)/7));
+tau_f       = 1102.5*exp(-(V+27)^2/225)+200/(1+exp((13-V)/10))+180/(1+exp((V+30)/10))+20;
+f2_inf      = 0.67/(1+exp((V+35)/7))+0.33;
+tau_f2      = 600*exp(-(V+25)^2/170)+31/(1+exp((25-V)/10))+16/(1+exp((V+30)/10));
+fCass_inf   = 0.6/(1+(Ca_ss/0.05)^2)+0.4;
+tau_fCass   = 80/(1+(Ca_ss/0.05)^2)+2;
+i_CaL       = par_g_CaL*d*f*f2*fCass*4*(V-15)*F^2/(R*T)*(0.25*Ca_ss*exp(2*(V-15)*F/(R*T))-Ca_o)/(exp(2*(V-15)*F/(R*T))-1);
+% Background Currents
+i_b_Ca      = g_bca*(V-E_Ca);
+i_b_Na      = g_bna*(V-E_Na);
+% Transient Outward Current
+if choice == 0
+% epi and M cell
+    s_inf   = 1/(1+exp((V+20)/5));
+    tau_s   = 85*exp(-(V+45)^2/320)+5/(1+exp((V-20)/5))+3;
+elseif choice == 1
+% endo
+    s_inf   = 1/(1+exp((V+28)/5));
+    tau_s   = 1000*exp(-(V+67)^2/1000)+8;
+end  
+r_inf       = 1/(1+exp((20-V)/6));
+tau_r       = 9.5*exp(-(V+40)^2/1800)+0.8;
+i_to        = g_to*r*s*(V-E_K);
+% Sodium/Potassium Pump Current
+i_NaK       = P_NaK*K_o/(K_o+K_mk)*Na_i/(Na_i+K_mNa)*1/(1+0.1245*exp(-0.1*V*F/(R*T))+0.0353*exp(-V*F/(R*T)));
+i_p_Ca      = g_pCa*Ca_i/(Ca_i+K_pCa);
+i_p_K       = g_pK*(V-E_K)/(1+exp((25-V)/5.98));
+% Sodium/Calcium Exchanger Current
+i_NaCa      = K_NaCa*(exp(gamma_0*V*F/(R*T))*Na_i^3*Ca_o-exp((gamma_0-1)*V*F/(R*T))*Na_o^3*Ca_i*alpha_0)/((Km_Nai^3+Na_o^3)*(Km_Ca+Ca_o)*(1+K_sat*exp((gamma_0-1)*V*F/(R*T))));
+% Calcium Dynamics
+kcasr       = max_sr-(max_sr-min_sr)/(1+(EC/Ca_SR)^2);
+k1          = k1_prime/kcasr;
+k2          = k2_prime*kcasr;
+O           = k1*(Ca_ss)^2*R_prime/(k3+k1*(Ca_ss)^2);
+i_rel       = V_rel*O*(Ca_SR-Ca_ss);
+i_up        = Vmax_up/(1+(K_up)^2/(Ca_i)^2);
+i_leak      = V_leak*(Ca_SR-Ca_i);
+i_xfer      = V_xfer*(Ca_ss-Ca_i);
+Ca_i_bufc   = 1/(1+Buf_c*K_buf_c/(Ca_i+K_buf_c)^2);
+Ca_sr_bufsr = 1/(1+Buf_sr*K_buf_sr/(Ca_SR+K_buf_sr)^2);
+Ca_ss_bufss = 1/(1+Buf_ss*K_buf_ss/(Ca_ss+K_buf_ss)^2);
+%% stimulus
+stim_duration  = 2;
+i_Stim         = stim_amplitude-stim_amplitude*(t>stim_duration);
+%% ODE system
+dydt = [-(i_K1+i_to+i_Kr+i_Ks+i_CaL+i_NaK+i_Na+i_b_Na+i_NaCa+i_b_Ca+i_p_K+i_p_Ca-i_Stim)/Cm;
+        (xr1_inf-Xr1)/tau_xr1;
+        (xr2_inf-Xr2)/tau_xr2;
+        (xs_inf-Xs)/tau_xs;
+        (m_inf-m)/tau_m;
+        (d_inf-d)/tau_d;
+        (f_inf-f)/tau_f;
+        (f2_inf-f2)/tau_f2;
+        (fCass_inf-fCass)/tau_fCass;
+        (s_inf-s)/tau_s;
+        (r_inf-r)/tau_r;
+        -k2*Ca_ss*R_prime+k4*(1-R_prime);
+        Ca_i_bufc*((i_leak-i_up)*V_sr/V_c+i_xfer-1*(i_b_Ca+i_p_Ca-2*i_NaCa)/(2*V_c*F));
+        Ca_sr_bufsr*(i_up-(i_rel+i_leak));
+        Ca_ss_bufss*(-1*i_CaL/(2*V_ss*F)+i_rel*V_sr/V_ss-i_xfer*V_c/V_ss);
+        -(i_Na+i_b_Na+3*i_NaK+3*i_NaCa)/(V_c*F);
+        ];
---
+++ comparison plots/simulation_modified_TP06_epi_M_endo.m
@@ -0,0 +1,127 @@
+clear all
+close all
+clc
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 0:2
+    if i == 0
+        % epi
+        cases = 'epi';
+    elseif i == 1
+        % endo
+        cases = 'end';
+    elseif i == 2   
+        % M
+        cases = 'M';
+    end
+%% parameters
+    if cases == 'epi'
+        % epi 
+        choice = 0;
+        g_Ks = 0.392;
+    elseif cases == 'end'
+        % endo
+        g_Ks = 0.392;
+        choice = 1;
+    elseif cases == 'M'
+        % M cell
+        choice = 0;   
+        g_Ks = 0.098;
+    end
+    g_Kr  = 0.153*0.18;
+    g_Na  = 14.838; 
+    g_K1  = 5.405; 
+    g_CaL = 3.98e-5*5;
+    K_i   = 138.3;
+    Cm    = 1;
+    stim  = 85;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% simulation
+    opts  = odeset('RelTol',1e-13,'AbsTol',1e-18);
+    tspan = [0 4000]; 
+    y0_ = [-86.709,0.00448,      0.0087,0.00155,            3.164e-5,0.8009,0.9778,0.9953,0.3212,         0.9068,0.00013,3.715,0.00036,10.355];
+    y0  = [-86.709,0.00448,0.476,0.0087,0.00155,            3.164e-5,0.8009,0.9778,0.9953,0.3212,2.235e-8,0.9068,0.00013,3.715,0.00036,10.355];
+    y01 = [-86.709,0.00448,0.476,0.0087,0.00155,0.7573, 0.7,3.164e-5,0.8009,0.9778,0.9953,0.3212,2.235e-8,0.9068,0.00013,3.715,0.00036,10.355,K_i];
+    y02 = [-86.709,0.00448,0.476,0.0087,0.00155,0.7573, 0.7,3.164e-5,0.8009,0.9778,0.9953,0.3212,2.235e-8,0.9068,0.00013,3.715,0.00036,10.355];
+    if cases == 'epi'
+        % epi 
+        [t_epi1,y_epi1]   = ode15s(@fun_modified_TP06_epi_M_endo_14d,tspan,y0_,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,stim,choice);
+        [t_epi,y_epi]     = ode15s(@fun_modified_TP06_epi_M_endo_16d,tspan,y0,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,stim,choice);
+        [t,y]             = ode15s(@fun_TP06_model,tspan,y01,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,choice);
+        [t_epi_,y_epi_]   = ode15s(@fun_modified_TP06_epi_M_endo_18d,tspan,y02,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,choice);
+    elseif cases == 'end'
+        % endo
+        [t_endo1,y_endo1] = ode15s(@fun_modified_TP06_epi_M_endo_14d,tspan,y0_,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,stim,choice);
+        [t_endo,y_endo]   = ode15s(@fun_modified_TP06_epi_M_endo_16d,tspan,y0,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,stim,choice);
+        [t1,y1]           = ode15s(@fun_TP06_model,tspan,y01,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,choice);
+        [t_endo_,y_endo_] = ode15s(@fun_modified_TP06_epi_M_endo_18d,tspan,y02,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,choice);
+    elseif cases == 'M'
+        % M cell
+        [t_M,y_M]       = ode15s(@fun_modified_TP06_epi_M_endo_16d,tspan,y0,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,52,choice);
+        [t_M1,y_M1]     = ode15s(@fun_modified_TP06_epi_M_endo_16d,tspan,y0,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,89,choice);
+        [t_M2,y_M2]     = ode15s(@fun_modified_TP06_epi_M_endo_14d,tspan,y0_,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,95,choice);
+        [t2,y2]         = ode15s(@fun_TP06_model,tspan,y01,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,choice);
+        [t_M_,y_M_]     = ode15s(@fun_modified_TP06_epi_M_endo_18d,tspan,y02,opts,g_Kr,g_Ks,g_Na,g_K1,g_CaL,K_i,choice);
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% plot
+figure;
+tiledlayout("vertical")
+nexttile
+hold on 
+h   = plot(t_epi,y_epi(:,1),'-','Linewidth',10,'Color',[0.3010 0.7450 0.9330]);
+h1  = plot(t,y(:,1),'--','Linewidth',10,'Color',[1 0 0]);
+h2  = plot(t_epi_,y_epi_(:,1),'-','Linewidth',4,'Color',[0 0 0]);
+title('epicardial cell','Interpreter','latex')
+set(gca,'Fontsize',50,'FontWeight','bold');
+ylabel('voltage $V$ (mV)','Interpreter','latex');
+L = legend([h,h1,h2],'16 dimensional model','TP06 model','18 dimensional model');
+set(L,'Interpreter','latex','Box','off','Fontsize',50,'FontWeight','bold','Location','southwest');
+ylim([-90 85])
+xlim([0 500])
+xticks([])
+nexttile
+hold on
+h   = plot(t_endo,y_endo(:,1),'-','Linewidth',10,'Color',[0.3010 0.7450 0.9330]);
+h1  = plot(t1,y1(:,1),'--','Linewidth',10,'Color',[1 0 0]);
+h2  = plot(t_endo_,y_endo_(:,1),'-','Linewidth',4,'Color',[0 0 0]);
+title('endocardial cell','Interpreter','latex')
+set(gca,'Fontsize',50,'FontWeight','bold');
+ylim([-90 85])
+xlim([0 500])
+xlabel('time $t$ (ms)','Interpreter','latex');
+ylabel('voltage $V$ (mV)','Interpreter','latex');
+L = legend([h,h1,h2],'16 dimensional model','TP06 model','18 dimensional model');
+set(L,'Interpreter','latex','Box','off','Fontsize',50,'FontWeight','bold','Location','southwest');
+
+figure;
+tiledlayout("vertical")
+nexttile
+hold on 
+h   = plot(t_M,y_M(:,1),'-','Linewidth',10,'Color',[0.3010 0.7450 0.9330]);
+h1  = plot(t2,y2(:,1),'--','Linewidth',10,'Color',[1 0 0]);
+h2  = plot(t_M_,y_M_(:,1),'-','Linewidth',4,'Color',[0 0 0]);
+title('M cell','Interpreter','latex')
+set(gca,'Fontsize',50,'FontWeight','bold');
+ylabel('voltage $V$ (mV)','Interpreter','latex');
+L = legend([h],'16 dimensional model with $\mathrm{I}_\mathrm{stim} = 52 \frac{pA}{pF}$');
+set(L,'Interpreter','latex','Box','off','Fontsize',50,'FontWeight','bold','Location','northeast');
+ylim([-90 85])
+xlim([0 1800])
+xticks([])
+nexttile
+hold on
+h   = plot(t_M1,y_M1(:,1),'-','Linewidth',10,'Color',[0.3010 0.7450 0.9330]);
+h1  = plot(t2,y2(:,1),'--','Linewidth',10,'Color',[1 0 0]);
+h2  = plot(t_M_,y_M_(:,1),'-','Linewidth',4,'Color',[0 0 0]);
+h3  = plot(t_M2,y_M2(:,1),':','Linewidth',4,'Color',[0.4660 0.6740 0.1880]);
+set(gca,'Fontsize',50,'FontWeight','bold');
+ylim([-90 85])
+xlim([0 1800])
+xlabel('time $t$ (ms)','Interpreter','latex');
+ylabel('voltage $V$ (mV)','Interpreter','latex');
+L1 = legend([h1,h2],'TP06 model','18 dimensional model');
+set(L1,'Interpreter','latex','Box','off','Fontsize',50,'FontWeight','bold','Location','southwest','NumColumns',1);
+ah1 = axes('position',get(gca,'position'),'visible','off');
+L2 = legend(ah1,[h,h3],'16 dimensional model with $\mathrm{I}_\mathrm{stim} = 89 \frac{pA}{pF}$','14 dimensional model with $\mathrm{I}_\mathrm{stim} = 95 \frac{pA}{pF}$');
+set(L2,'Interpreter','latex','Box','off','Fontsize',50,'FontWeight','bold','Location','northeast');
